ce24776be56b085c6de41e86b1291443c61b57e0
Elena <lenatolmaceva20@gmail.com>
2025-05-20 17:28:54 +0200

    Update admin-script.js

--- vercel-public/admin-script.js
+++ vercel-public/admin-script.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 // Защита от повторной инициализации
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
-const db = getFirestore(app);
+
 
 // Получаем контейнер для вывода заказов
 const ordersContainer = document.getElementById("orders-container");
